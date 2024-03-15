8e4894a1fc8a3b0014a7967c22d3cd200f427f0d
Aysgl Avcu <aysglavcu@Aysgls-MacBook-Pro.local>
2024-03-15 15:41:30 +0300

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,38 @@
 const http = require("http");
+const cors = require("cors"); // cors paketini import edin
 
 const postReq = require("./methods/postReq");
 const deleteReq = require("./methods/deleteReq");
 const getReq = require("./methods/getReq");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  cors()(req, res, () => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
-  switch (req.method) {
-    case "OPTIONS":
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-      );
-
-      res.end();
-      break;
-    case "GET":
-      getReq(req, res);
-      break;
-    case "POST" || "OPTIONS":
-      postReq(req, res);
-      break;
-    case "DELETE":
-      deleteReq(req, res);
-      break;
-    default:
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "application/json");
-      res.write(JSON.stringify({ message: "Default" }));
-      res.end();
-  }
+    switch (req.method) {
+      case "OPTIONS":
+        res.setHeader(
+          "Access-Control-Allow-Methods",
+          "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+        );
+        res.end();
+        break;
+      case "GET":
+        getReq(req, res);
+        break;
+      case "POST" || "OPTIONS":
+        postReq(req, res);
+        break;
+      case "DELETE":
+        deleteReq(req, res);
+        break;
+      default:
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "application/json");
+        res.write(JSON.stringify({ message: "Default" }));
+        res.end();
+    }
+  });
 });
 
 const port = 8080;
